be84847e6c753ceb14204642aa058e5208557ffa
hideaki1979 <syumeikyo2000@yahoo.co.jp>
2024-10-23 23:49:37 +0900

    メモ帳アプリ新規作成

---
+++ js/edit_script.js
@@ -0,0 +1,51 @@
+window.addEventListener('load', function(){
+    const url = this.location.search;
+    // console.log(url);
+    const param = url.split('=');
+    const key = param[1];
+    if(key !== ""){
+        const value = this.localStorage.getItem(key);
+        // console.log(key);
+        $("#title").val(key);
+        $("#memo").val(value);
+    }
+});
+
+// 登録ボタン押下時
+$("#regist").on("click", function(){
+    const key = $("#title").val();
+    const value = $("#memo").val();
+    // 重複キーチェック
+    for (let i = 0; i < localStorage.length; i++){
+        if (key === localStorage.key(i)){
+            alert(`キーが既に存在しています。キー：${key}`);
+            return;
+        }
+    }
+    localStorage.setItem(key, value);
+    alert(`メモ情報を登録しました。キー：${key}`);
+    mainScreen()
+});
+
+// 編集ボタン押下時
+$("#edit").on("click", function(){
+    // メモを更新
+    const key = $("#title").val();
+    const value = $("#memo").val();
+    localStorage.setItem(key, value);
+    alert(`メモ情報を登録しました。キー：${key}`);
+    mainScreen();
+});
+
+// 削除ボタン押下時
+$("#delete").on("click", function(){
+    const key = $("#title").val();
+    localStorage.removeItem(key);
+    alert(`メモ情報を登録しました。キー：${key}`)
+    mainScreen();
+});
+
+// メイン画面へ遷移
+function mainScreen() {
+    location.href = "index.html";
+}
---
+++ js/script.js
@@ -0,0 +1,49 @@
+'use strict';
+
+// 初期処理
+window.addEventListener('load', function(){
+    listDisplay();
+});
+
+// 初期登録
+$("#init-regist").on("click", function(){
+    localStorage.setItem("111", "aaaaa");
+    localStorage.setItem("222", "bbbbb");
+    localStorage.setItem("333", "ccccc");
+    localStorage.setItem("444", "ddddd");
+    localStorage.setItem("555", "eeeee");
+    localStorage.setItem("666", "fffff");
+    localStorage.setItem("777", "ggggg");
+    localStorage.setItem("888", "hhhhh");
+    localStorage.setItem("999", "iiiii");
+    localStorage.setItem("101", "jjjjj");
+    localStorage.setItem("102", "kkkkk");
+    localStorage.setItem("103", "lllll");
+    listDisplay();
+});
+
+// 全件削除ボタン押下時
+$("#all-delete").on("click", function(){
+    localStorage.clear();
+    $("#memolist").empty();
+    $("#title").val("");
+    $("#memo").val("");
+});
+
+// thクリック時
+// 動的に追加されたタグは親要素へイベント委譲（イベントデリゲーションというのが必要らしい・・・）
+$("#memolist").on("click", "th", function(){
+    const key = $(this).text();
+    // console.log(key);
+    location.href = `edit.html?key=${key}`;
+});
+
+// tableリスト出力する
+function listDisplay() {
+    for(let i = 0; i < localStorage.length; i++){
+        const key = localStorage.key(i);
+        const value = localStorage.getItem(key);
+        const html = `<tr><th>${key}</th><td>${value}</td></tr>`;
+        $("#memolist").append(html);
+    }
+};
